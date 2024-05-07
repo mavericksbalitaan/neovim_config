1bdf0c4a1735ebecff200b00d64c1f9fba437c38
Mavericks Balitaan <balitaanmavericks@gmail.com>
2024-05-07 14:08:09 +0800

    Initial commit

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,13 @@
+return {
+	"rmagatti/auto-session",
+	config = function()
+		require("auto-session").setup({
+			pre_save_cmds = { "Neotree close" },
+			post_restore_cmds = { "Neotree filesystem reveal left toggle" },
+		})
+		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+		vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
+			noremap = true,
+		})
+	end,
+}
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			open_mapping = "<leader>=",
+			direction = "float",
+			auto_scroll = true,
+			vim.keymap.set("n", "<leader>lt", ":TermSelect<CR>"),
+			vim.keymap.set("n", "<leader>fp", ":2TermExec cmd='prettier % --write' <CR>"),
+		})
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
+
+		vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+	end,
+}
