c9d6068cb4db48dc6b3097e9129ef214074e9c85
Mavericks Balitaan <balitaanmavericks@gmail.com>
2024-11-28 13:58:40 +0800

    Format completion box with lspkind

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -18,6 +18,9 @@ return {
 		"github/copilot.vim",
 	},
 	{
+		"hrsh7th/cmp-emoji",
+	},
+	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
@@ -29,6 +32,7 @@ return {
 		config = function()
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
 
 			require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -85,8 +89,37 @@ return {
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 					{ name = "path" },
-					{ name = "buffer",  keyword_length = 5 },
+					{ name = "buffer", keyword_length = 5 },
+					{ name = "emoji" },
 				}),
+				formatting = {
+					format = lspkind.cmp_format({
+						menu = {
+							nvim_lua = "[Lua]",
+							nvim_lsp = "[LSP]",
+							luasnip = "[LuaSnip]",
+							path = "[Path]",
+							buffer = "[Buffer]",
+							emoji = "[Emoji]",
+						},
+						-- mode = "symbol", -- show only symbol annotations
+						maxwidth = {
+							-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+							-- can also be a function to dynamically calculate max width such as
+							-- menu = function() return math.floor(0.45 * vim.o.columns) end,
+							menu = 50, -- leading text (labelDetails)
+							abbr = 50, -- actual suggestion item
+						},
+						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+						show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+						-- The function below will be called before any actual modifications from lspkind
+						-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+						before = function(entry, vim_item)
+							return vim_item
+						end,
+					}),
+				},
 			})
 		end,
 	},
