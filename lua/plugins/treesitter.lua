4fc1b7133b65e510fd1588a6c36772bd9e9c94a7
Mavericks Balitaan <balitaanmavericks@gmail.com>
2025-01-12 04:53:27 +0800

    Install additional treesitter parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "javascript", "html", "css", "json", "graphql", "python" },
+			ensure_installed = { "lua", "javascript", "html", "css", "json", "graphql", "python", "regex", "bash" },
 			highlight = {
 				enable = true,
 			},
