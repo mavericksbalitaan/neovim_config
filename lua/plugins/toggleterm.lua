d356201f58124e235e8fa96f9005f9c0b9d94689
Mavericks Balitaan <balitaanmavericks@gmail.com>
2025-01-14 08:49:40 +0800

    Change direction of toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,22 +1,23 @@
 return {
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	config = function()
-		require("toggleterm").setup({
-			open_mapping = "<leader>=",
-			direction = "float",
-			auto_scroll = true,
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = "<leader>=",
+      direction = "horizontal",
+      size = 20,
+      auto_scroll = true,
       autochdir = true,
-			vim.keymap.set("n", "<leader>lt", ":TermSelect<CR>"),
-			vim.keymap.set("n", "<leader>fp", ":2TermExec cmd='prettier % --write' <CR>"),
-		})
-		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+      vim.keymap.set("n", "<leader>lt", ":TermSelect<CR>"),
+      vim.keymap.set("n", "<leader>fp", ":2TermExec cmd='prettier % --write' <CR>"),
+    })
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({ cmd = "lazygit", direction = "float", hidden = true })
 
-		function _lazygit_toggle()
-			lazygit:toggle()
-		end
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
 
-		vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
-	end,
+    vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+  end,
 }
