e20e9c33379ebc93673efe5db463125a211af7dc
Mavericks Balitaan <balitaanmavericks@gmail.com>
2025-03-14 22:46:27 +0800

    Add file preview mode in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,17 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+		require("neo-tree").setup({
+			window = {
+				mappings = {
+					["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
+					["l"] = "focus_preview",
+					["<C-b>"] = { "scroll_preview", config = { direction = 10 } },
+					["<C-f>"] = { "scroll_preview", config = { direction = -10 } },
+				},
+			},
+		})
+
 		vim.keymap.set("n", "<leader>-", ":Neotree filesystem reveal left toggle<CR>")
 	end,
 }
