72d2b684675f990417e32e7f52bf9a30ebf4a1bd
Mavericks Balitaan <balitaanmavericks@gmail.com>
2025-09-06 21:52:03 +0800

    Remove noice plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,8 +9,6 @@ return {
       sections = {
         lualine_x = {
           {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
             color = { fg = "#ff9e64" },
           }
         },
