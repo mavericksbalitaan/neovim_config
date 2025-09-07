c0866f91338e99bc57f74f0efde39a2b55b6c568
Mavericks Balitaan <balitaanmavericks@gmail.com>
2025-09-07 10:42:01 +0800

    Add typescript and prisma in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,19 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "javascript", "html", "css", "json", "graphql", "python", "regex", "bash" },
+			ensure_installed = {
+				"lua",
+				"javascript",
+				"typescript",
+				"html",
+				"css",
+				"json",
+				"graphql",
+				"python",
+				"prisma",
+				"regex",
+				"bash",
+			},
 			highlight = {
 				enable = true,
 			},
@@ -12,18 +24,5 @@ return {
 				enable = true,
 			},
 		})
-
-		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-		parser_config.liquid = {
-			install_info = {
-				url = "~/tree-sitter-liquid", -- local path or git repo
-				files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-				-- optional entries:
-				branch = "main", -- default branch in case of git repo if different from master
-				generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-				requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-			},
-			filetype = "liquid", -- if filetype does not match the parser name
-		}
 	end,
 }
